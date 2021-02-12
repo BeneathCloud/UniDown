32d1aaad8ad7e86f3f34b72ffbfbe7dbab9d198d
Laz <undercloud@yeah.net>
2021-02-13 05:08:14 +0900

    fix instagram

--- instagram.js
+++ instagram.js
@@ -9,13 +9,14 @@ const config = require('./config.js')
 const username = config.get('instagram.username').value()
 const password = config.get('instagram.password').value()
 const following = config.get('instagram.following')
+const downloader = config.get('instagram.pathToInstaloader').value()
 
 async function downloadProfile(url, path) {
     const profile = urlParser.parse(url).pathname.split('/').filter(x => x!=='')[0]
 
     status.setDownloading(profile, url)
 
-    exec(`cd ${path}; instaloader --login ${username} --password ${password} ${profile}`, (error, stdout, stderr) => {
+    exec(`cd ${path}; ${downloader} --login ${username} --password ${password} ${profile}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
             status.setFailed(profile, error)
@@ -44,7 +45,7 @@ async function update(profile, path) {
 
     status.setDownloading(profile, "updating...")
     
-    exec(`cd ${path}; instaloader --login ${username} --password ${password} --fast-update ${profile}`, (error, stdout, stderr) => {
+    exec(`cd ${path}; ${downloader} --login ${username} --password ${password} --fast-update ${profile}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
             status.setFailed(profile, error)
@@ -70,7 +71,7 @@ async function updateAllFollowing(path) {
     status.setDownloading("updating all following...")
     
     const followList = following.value()
-    exec(`cd ${path}; instaloader --login ${username} --password ${password} --fast-update ${followList.join(' ')}`, (error, stdout, stderr) => {
+    exec(`cd ${path}; ${downloader} --login ${username} --password ${password} --fast-update ${followList.join(' ')}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
             status.setFailed("update all", error)
