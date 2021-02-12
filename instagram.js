bacda30a50255d4d5f659f000438274a01393912
Laz <undercloud@yeah.net>
2021-02-13 04:45:41 +0900

    add status

--- instagram.js
+++ instagram.js
@@ -12,49 +12,73 @@ const following = config.get('instagram.following')
 
 async function downloadProfile(url, path) {
     const profile = urlParser.parse(url).pathname.split('/').filter(x => x!=='')[0]
+
+    status.setDownloading(profile, url)
+
     exec(`cd ${path}; instaloader --login ${username} --password ${password} ${profile}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
+            status.setFailed(profile, error)
             return
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`)
+            status.setFailed(profile, stderr)
             return
         }
         console.log(`stdout: ${stdout}`)
         following.push(profile).write()
         console.log('[Download Completed: ]' + profile)
+
+        status.removeDownloading(profile)
+        status.setDownloaded(profile, url)
     })
 }
 
 async function update(profile, path) {
+
+    status.setDownloading(profile, "updating...")
+    
     exec(`cd ${path}; instaloader --login ${username} --password ${password} --fast-update ${profile}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
+            status.setFailed(profile, error)
             return
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`)
+            status.setFailed(profile, stderr)
             return
         }
         console.log(`stdout: ${stdout}`)
         console.log('[Update Completed: ]' + profile)
+
+        status.removeDownloading(profile)
+        status.setDownloaded(profile, "updated")
     })
 }
 
 async function updateAllFollowing(path) {
+
+    status.setDownloading("updating all following...")
+    
     const followList = following.value()
     exec(`cd ${path}; instaloader --login ${username} --password ${password} --fast-update ${followList.join(' ')}`, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
+            status.setFailed("update all", error)
             return
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`)
+            status.setFailed("update all", stderr)
             return
         }
         console.log(`stdout: ${stdout}`)
         console.log('[Update All Completed: ]' + followList.join(', '))
+
+        status.removeDownloading("updating all following...")
+        status.setDownloaded("updated all following")
     })
 }
 
