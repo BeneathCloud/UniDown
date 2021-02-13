1cdf68685a7b72a7734023e90bbff561acbe7435
Laz <undercloud@yeah.net>
2021-02-13 17:52:56 +0900

    add duplicate check for xvideos

--- xvideos.js
+++ xvideos.js
@@ -1,13 +1,17 @@
 const m3u8Downloader = require('./m3u8-downloader.js')
 const xvideos = require('@rodrigogs/xvideos')
 const status = require('./status.js')
+const utils = require('./utils.js')
 
 async function download(url, path) {
     try {
 
         const info = await xvideos.videos.details({ url: url })
         const title = info.title.replaceAll('/', '\\')
-        const pathName = path + '/' + title + '.mp4'
+        const fileName = title + '.mp4'
+        const pathName = path + '/' + fileName
+
+        if (utils.isDuplicate(fileName, path)) throw `file ${fileName} exists in ${path}`
 
         status.setDownloading(title, url)
         await m3u8Downloader.download(info.files.HLS, pathName)
